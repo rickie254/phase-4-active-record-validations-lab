07aa017a2200ce6d4eddb13a902b1c0be40c12b1
rickie <rickiemaweu@gmail.com>
2022-10-03 02:59:05 +0300

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence:true
+    validates :content, length:{minimum:250}
+    validates :summary, length:{maximum:250}
+    validates :category, inclusion:{in:["Fiction", "Non-Fiction"]}
+    validate :clickbait?
+
+     def clickbait?
+    if [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ].none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
